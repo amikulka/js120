1e0a37b9c2dcb800b6db14421a02c53e46e5752a
Aaron Mikulka <aaronjmikulka@gmail.com>
2021-11-02 16:49:08 -0700

    started lesson 1

---
+++ lesson1/practice_objectsAndFactories/problem5.js
@@ -0,0 +1,19 @@
+function createBook(title, author, read = false) {
+  return {
+    title,
+    author,
+    read,
+
+    getDescription() {
+      return `${this.title} was written by ${this.author}.`;
+    }
+  };
+}
+
+let book1 = createBook('Mythos', 'Stephen Fry');
+let book2 = createBook('Me Talk Pretty One Day', 'David Sedaris');
+let book3 = createBook("Aunts aren't Gentlemen", 'PG Wodehouse');
+
+book1.getDescription();  // "Mythos was written by Stephen Fry."
+book2.getDescription();  // "Me Talk Pretty One Day was written by David Sedaris."
+book3.getDescription();  // "Aunts aren't Gentlemen was written by PG Wodehouse"
